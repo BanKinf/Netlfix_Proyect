2a8577de95519b854296039da4f032a98afc1277
_ignacio.dev_ <protec83007@gmail.com>
2022-05-15 10:54:04 -0300

    Banner add

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,25 @@
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { Movie } from '../typings';
 
-function Banner() {
-  return <div>
-      {/* <div>
-          <Image/>
-      </div> */}
-  </div>
+interface Props {
+    netflixOriginals: Movie[]
+}
+
+function Banner({netflixOriginals}: Props) {
+    const [movie, setMovie] = useState<Movie | null>(null)
+
+    useEffect(() => {
+        setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
+    },[netflixOriginals]);
+
+  return (
+    <div>
+      <div>
+        <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}/>
+      </div>
+    </div>
+  )
 }
 
 export default Banner
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
-
 import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import { Movie } from '../typings'
 import requests from '../utils/requests'
 
-const Home = () => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -14,7 +34,7 @@ const Home = () => {
       
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={netflixOriginals}/>
         <section>
           {/* Row */}
           {/* Row */}
